59f0807b8d835731768ce224e7b2200a3e9f7ea2
conradolucas <oconradolucas@gmail.com>
2019-05-29 18:25:13 -0300

    text 3

--- src/app/thank.js
+++ src/app/thank.js
@@ -1,10 +1,11 @@
 const speaker = 'Lucas';
 const community = 'JSSP';
-    const company = 'ESX';
+const company = 'ESX';
 
-    StyleSheet.init();
+StyleSheet.init();
 
-    StyleSheet.create('body', `
+
+StyleSheet.create('body', `
       padding: 50px 0 0;
       color: #000;
       font-size: 30px;
@@ -13,23 +14,21 @@ const community = 'JSSP';
       text-align: center;
     `);
 
-    StyleSheet.create(".entity", `
+StyleSheet.create('.entity', `
       color: #189ae6;
       font-weight: bold;
       text-align: center;
-    `)
+    `);
 
 
-    function buildThank(template, ...values) {
-        return template.reduce((accumulator, part, i) => {
-            return ` 
+function buildThank(template, ...values) {
+  return template.reduce((accumulator, part, i) => ` 
                 ${accumulator}
                 <span class="entity">${values[i - 1].toUpperCase()}</span>
                 ${part}
-            `
-        });
-    }
+            `);
+}
 
-    const thank = buildThank`Oi :) sou o ${speaker} é uma honra pra mim estar aqui com voces do ${community} na ${company}!`;
+const thank = buildThank`Oi :) sou o ${speaker} é uma honra pra mim estar aqui com voces do ${community} na ${company}!`;
 
-    document.body.innerHTML = thank;
+document.body.innerHTML = thank;
